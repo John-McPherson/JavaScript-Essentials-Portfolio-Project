c986ead7cc3e939f279043d5c8f0acbb249eccc1
John-McPherson <john-mac1989@hotmail.co.uk>
2021-11-08 17:38:59 +0000

    add start game event and clear game event to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,32 @@ let control = document.getElementsByClassName('control');
 for (let x = 0; x < control.length; x++) {
     control[x].addEventListener("click", wrongAns);
 }
-
+/**
+*Called when the user enters an incorrect guess. 
+*Turns the pressed key red and advances the hangman image by one
+*/
 function wrongAns(event) {
     event.preventDefault();
     event.target.classList.add("wrong");
+}
+
+/**
+* The fuction run when user clicks on the start game button.
+* It clears the game board and generates a new word to be guessed
+*/
+function startGame(event) {
+    event.preventDefault();
+    let newGameBtn = document.getElementById("new-game");
+    newGameBtn.innerHTML = "New Game"
+    clearGame();
+
+}
+/**
+* Clears the wrong and right answers
+*/
+function clearGame(){
+    for (let x = 0; x < control.length; x++) {
+        control[x].classList.remove("wrong");
+        control[x].classList.remove("right");
+    }
 }
