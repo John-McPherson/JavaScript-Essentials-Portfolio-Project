9142b7fff33f381c0b31392646d76f82da0d94e4
John-McPherson <john-mac1989@hotmail.co.uk>
2021-11-24 14:12:24 +0000

    fix bug causing console log errors when pressing modifier keys

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,8 +14,8 @@ let diffSelect = document.getElementsByClassName('diff-select');
 for (let x = 0; x < diffSelect.length; x++) {
     diffSelect[x].addEventListener("click", startGame);
 }
-window.addEventListener('keydown',keyControls);
-document.getElementById('mute-button').addEventListener('change',muteSfx);
+window.addEventListener('keydown', keyControls);
+document.getElementById('mute-button').addEventListener('change', muteSfx);
 var wrong = 0;
 var right = 0;
 var word = "start";
@@ -32,11 +32,11 @@ var hard = ['jazz', 'conscience', 'audible', 'diamond', 'industrial', 'buried', 
  */
 function wrongAns(event) {
     event.preventDefault();
-        event.target.classList.add("wrong");
-        event.target.classList.add("guessed");
-        let img = document.getElementById('hangman');
-        wrong++;
-        img.innerHTML = `<img src="./assets/images/hangman${wrong}.png" alt="an partly drawn image of a man hanging from a set of gallows drawn in the style of the childerns game hangman indicating that the players have ${10-wrong} guesses left">`;
+    event.target.classList.add("wrong");
+    event.target.classList.add("guessed");
+    let img = document.getElementById('hangman');
+    wrong++;
+    img.innerHTML = `<img src="./assets/images/hangman${wrong}.png" alt="an partly drawn image of a man hanging from a set of gallows drawn in the style of the childerns game hangman indicating that the players have ${10-wrong} guesses left">`;
 }
 /**
  *Called when the user enters an correct guess. 
@@ -55,7 +55,7 @@ function startGame(event) {
     event.preventDefault();
     difficulty = this.getAttribute("data-diff");
     let newGameBtn = document.getElementsByClassName('new-game');
-    for(let x of newGameBtn){
+    for (let x of newGameBtn) {
         console.log(x.innerHTML);
         let html = "New Game";
         x.innerHTML = html;
@@ -101,41 +101,42 @@ function newWord() {
  */
 function checkGuess(event) {
     event.preventDefault();
-    if (event.target.classList.contains("guessed")){    
+    if (event.target.classList.contains("guessed")) {
         // do nothing
-    }
-    else {
-    resetTime();
-    let guess = this.getAttribute("data-key");
-    let correct = false;
-    let arr = word.split("");
-    for (let x of arr) {
-        if (x === guess) {
-            correct = true;
-        }
-    }
-    if (correct === true) {
-        rightAns(event);
-        let span = document.getElementsByClassName('letter');
-        for (let y of span) {
-           let letter = y.getAttribute("data-letter");
-            if (letter === guess) {
-                y.classList.remove("not-guessed");
-                if (mute === false){
-                    let audio = document.getElementById('correct-sfx');
-                    audio.play();}
-                    right++;
+    } else {
+        resetTime();
+        let guess = this.getAttribute("data-key");
+        let correct = false;
+        let arr = word.split("");
+        for (let x of arr) {
+            if (x === guess) {
+                correct = true;
             }
         }
-    } else {
-        let audio = document.getElementById('wrong-sfx');
-        if (mute === false){
-            audio.play();
+        if (correct === true) {
+            rightAns(event);
+            let span = document.getElementsByClassName('letter');
+            for (let y of span) {
+                let letter = y.getAttribute("data-letter");
+                if (letter === guess) {
+                    y.classList.remove("not-guessed");
+                    if (mute === false) {
+                        let audio = document.getElementById('correct-sfx');
+                        audio.play();
+                    }
+                    right++;
+                }
+            }
+        } else {
+            let audio = document.getElementById('wrong-sfx');
+            if (mute === false) {
+                audio.play();
+            }
+            wrongAns(event);
         }
-        wrongAns(event);
+        checkEnd();
     }
-    checkEnd();
-}}
+}
 /**
  * After the players guess has been checked it checks the game state to see if the win/loss condition has been met.
  */
@@ -157,10 +158,10 @@ function genWord() {
     } else {
         word = easy[Math.floor(Math.random() * easy.length)];
     }
-    if (word.length > 7){
+    if (word.length > 7) {
         let wordContainer = document.getElementById('word');
         wordContainer.classList.add('long');
-    }else if (word.length > 9){
+    } else if (word.length > 9) {
         let wordContainer = document.getElementById('word');
         wordContainer.classList.add('really-long');
     }
@@ -185,7 +186,7 @@ function openModal(event) {
  * closes all modals
  */
 function closeModal() {
-    let close =document.getElementsByClassName('modal');
+    let close = document.getElementsByClassName('modal');
     for (let x of close) {
         x.classList.add('modal-close');
     }
@@ -194,67 +195,70 @@ function closeModal() {
  * allows the player to control the game through the keyboard 
  * 
  */
-function keyControls(event){
+function keyControls(event) {
     let key = event.key;
-    if(key === 'Enter'){
+    if (key === 'Enter') {
         closeModal();
         let open = document.getElementById('diff-select');
         open.classList.remove('modal-close');
-    }else if ((/[a-zA-Z]/).test(key)){
+    } else if ((/[a-zA-Z]/).test(key)) {
         let pressed = document.querySelectorAll(`[data-key='${key}']`);
-        pressed[0].click();
-    }else{
+        if (Boolean(pressed[0])){
+            pressed[0].click();
+        }
+    } else {
         // do nothing
     }
 }
 /**
  * allows the player to mute the games sfxs
  */
-function muteSfx(){
-    if(mute === false){
+function muteSfx() {
+    if (mute === false) {
         mute = true;
-    }else {
-        mute =false;
+    } else {
+        mute = false;
     }
 }
 /**
  * updates the timer and updates the fail condition if the timer reaches zero
  */
-function updateTimer(){
-    if (gameStart === true){
-    time--;
-    let timer = document.getElementsByClassName('timer');
-    timer[0].innerHTML = `<p>Time: ${time}</p>`;
-    if (time <=1){
-        let img = document.getElementById('hangman');
-        wrong++;
-        img.innerHTML = `<img src="./assets/images/hangman${wrong}.png" alt="">`;
-        resetTime();
-        checkEnd();
-        if (mute === false){
-            let audio = document.getElementById('wrong-sfx');
-            audio.play();
+function updateTimer() {
+    if (gameStart === true) {
+        time--;
+        let timer = document.getElementsByClassName('timer');
+        timer[0].innerHTML = `<p>Time: ${time}</p>`;
+        if (time <= 1) {
+            let img = document.getElementById('hangman');
+            wrong++;
+            img.innerHTML = `<img src="./assets/images/hangman${wrong}.png" alt="">`;
+            resetTime();
+            checkEnd();
+            if (mute === false) {
+                let audio = document.getElementById('wrong-sfx');
+                audio.play();
+            }
         }
-    }}
+    }
 }
 /**
  * calls the updateTimer function every second
  */
-window.setInterval(function() {
+window.setInterval(function () {
     updateTimer();
-  }, 1000);
+}, 1000);
 /**
  * resets the timer
  */
-  function resetTime(){
+function resetTime() {
     time = 11;
     let timer = document.getElementsByClassName('timer');
     timer[0].innerHTML = `<p>Time: 10</p>`;
-  }
+}
 /**
  * run when the player runs out of guesses 
  */
-  function gameOver(){
+function gameOver() {
     for (let x = 0; x < control.length; x++) {
         control[x].classList.add("guessed");
     }
@@ -264,11 +268,11 @@ window.setInterval(function() {
     gameOverText.innerHTML = html;
     gameOver.classList.remove('modal-close');
     gameStart = false;
-  }
-  /**
-   * run when the player has correctly guessed the word.
-   */
-  function congrats(){
+}
+/**
+ * run when the player has correctly guessed the word.
+ */
+function congrats() {
     for (let x = 0; x < control.length; x++) {
         control[x].classList.add("guessed");
     }
